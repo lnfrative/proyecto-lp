9b5854037ad34f743bead318ce899dfb1a6b09bc
stephcarol <85514342+stephcarol@users.noreply.github.com>
2024-08-18 17:05:27 -0500

    Update ReservacionesController.php
    division de metodos

--- src/Controller/ReservacionesController.php
+++ src/Controller/ReservacionesController.php
@@ -3,8 +3,53 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
 
 class ReservacionesController extends AbstractController
 {
-   
+    // Christopher Díaz
+    #[Route('/reservaciones/crear', name: 'app_reservaciones_crear', methods: ['POST'])]
+    public function crear(Request $request, EntityManagerInterface $entityManager): Response
+    {
+    
+    }
+
+    // Stephany
+    #[Route('/reservaciones/aprobadas', name: 'app_reservaciones_aprobadas', methods: ['GET'])]
+    public function aprobadas(Request $request, EntityManagerInterface $entityManager): Response
+    {
+      
+    }
+
+    // Stephany
+    #[Route('/reservaciones/aprobar', name: 'app_reservaciones_aprobar', methods: ['POST'])]
+    public function aprobar(Request $request, EntityManagerInterface $entityManager): Response
+    {
+      
+    }
+
+    // Stephany
+    #[Route('/reservaciones/cancelar', name: 'app_reservaciones_cancelar', methods: ['POST'])]
+    public function cancelar(Request $request, EntityManagerInterface $entityManager): Response
+    {
+      
+    }
+
+    // Stephany
+    #[Route('/reservaciones/rechazar', name: 'app_reservaciones_rechazar', methods: ['POST'])]
+    public function rechazar(Request $request, EntityManagerInterface $entityManager): Response
+    {
+       
+    }
+
+    // Stephany
+    #[Route('/reservaciones/consultar', name: 'app_reservaciones_consultar', methods: ['GET'])]
+    public function consultar(Request $request, EntityManagerInterface $entityManager): Response
+    {
+       
+    }
 }
