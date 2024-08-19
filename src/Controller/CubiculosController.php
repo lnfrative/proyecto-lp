b55a4010674e00ec5748d2095ea32d3cca111448
Christopher Díaz <38027442+lnfrative@users.noreply.github.com>
2024-08-18 23:06:03 -0500

    cubiculos disponibles partial

--- src/Controller/CubiculosController.php
+++ src/Controller/CubiculosController.php
@@ -16,6 +16,18 @@ use DateTime;
 
 class CubiculosController extends AbstractController
 {
+    // Christopher
+    #[Route('/cubiculos/disponibles', name: 'app_cubiculos_disponibles', methods: ['GET'])]
+    public function disponibles(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        // TODO
+        // $time = DateTime::createFromFormat('H:i', $request->query->get('time'));
+        // $horario = $entityManager->getRepository(Horario::class)->findBy(array('hora' => $time));
+        // return $this->json($horario);
+
+
+    }
+    
     // José Baidal
     #[Route('/cubiculos/creados', name: 'app_cubiculos_creados', methods: ['GET'])]
     public function creados(Request $request, EntityManagerInterface $entityManager): Response
